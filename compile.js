fb71803d1aa839725a956afa84bcd3aa75d72274
Bence Dányi <bence@danyi.me>
2015-01-26 09:47:17 +0100

    Use old resolve semantics for relative paths

--- compile.js
+++ compile.js
@@ -11,10 +11,12 @@ P.prototype = Object.create(jade.Parser.prototype);
 P.prototype.constructor = P;
 
 P.prototype.resolvePath = function (path, purpose) {
+  if (path[0] === '.') {
+    return jade.Parser.prototype.resolvePath.apply(this, arguments);
+  }
   return resolve.sync(path, {
     extensions: ['.jade'],
   });
-  return jade.Parser.prototype.resolvePath.apply(this, arguments);
 };
 
 console.log(jade.renderFile('./index.jade', {
